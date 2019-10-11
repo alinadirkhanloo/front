f0ae24ab010e4037d8c2b89642307eb28a14eab4
jolan <jolan657673@gmail.com>
2019-10-11 23:28:16 +0330

    dsf

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -22,7 +22,8 @@ export class ApiService {
 
   httpHeaders = new HttpHeaders({
     'Content-Type':'application/json',
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcwMjE3NDUyLCJqdGkiOiI2NzYwNDE2YTU2NzQ0YmZiYWNlNTU5NjBkMTIzZjJlZCIsInVzZXJfaWQiOjF9.eZC6rVasPAgCazZucdPLt3KbUHMrZqyxpa1xta4WZuQ'  })
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcwMjE3NDUyLCJqdGkiOiI2NzYwNDE2YTU2NzQ0YmZiYWNlNTU5NjBkMTIzZjJlZCIsInVzZXJfaWQiOjF9.eZC6rVasPAgCazZucdPLt3KbUHMrZqyxpa1xta4WZuQ'  
+  })
 
   login_user(email, pass){
     const user = {
@@ -33,42 +34,9 @@ export class ApiService {
       console.log(res,email,pass);
     })
   }
-
-  // import { Injectable } from '@angular/core';
-  // import { HttpClient, HttpResponse } from '@angular/common/http';
-  // import { Observable } from 'rxjs';
-  
-  // import { SERVER_API_URL } from 'app/app.constants';
-  // import { createRequestOption } from 'app/shared';
-  // import { ICategory } from 'app/shared/model/category.model';
-  
-  // type EntityResponseType = HttpResponse<ICategory>;
-  // type EntityArrayResponseType = HttpResponse<ICategory[]>;
-  
-  // @Injectable({ providedIn: 'root' })
-  // export class CategoryService {
-  //   public resourceUrl = SERVER_API_URL + 'api/categories';
-  
-  //   constructor(protected http: HttpClient) {}
-  
-  //   create(category: ICategory): Observable<EntityResponseType> {
-  //     return this.http.post<ICategory>(this.resourceUrl, category, { observe: 'response' });
-  //   }
-  
-  //   update(category: ICategory): Observable<EntityResponseType> {
-  //     return this.http.put<ICategory>(this.resourceUrl, category, { observe: 'response' });
-  //   }
-  
-  //   find(id: number): Observable<EntityResponseType> {
-  //     return this.http.get<ICategory>(`${this.resourceUrl}/${id}`, { observe: 'response' });
-  //   }
-  
-  //   query(req?: any): Observable<EntityArrayResponseType> {
-  //     const options = createRequestOption(req);
-  //     return this.http.get<ICategory[]>(this.resourceUrl, { params: options, observe: 'response' });
-  //   }
-  
-  //   delete(id: number): Observable<HttpResponse<any>> {
-  //     return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response' });
-  //   }
+  getUsers(){
+    this.http.get<any>("http://127.0.0.1:8000/users/",{headers:this.httpHeaders}).subscribe((res) => {
+      console.log(res);
+    }) 
+  }
 }
