9bdb0b88714cb77fdccf41c3c11591f30fa17411
ali <alinadirkhanlo.98@gmail.com>
2019-11-04 12:09:48 +0000

    map panel

--- src/app/components/pages/dashboard/dashboard-routing.module.ts
+++ src/app/components/pages/dashboard/dashboard-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { MapComponent } from '../../charts/map/map.component';
+import { DatePickerComponent } from '../../date-picker/date-picker.component';
 
 
 const routes: Routes = [{path: '', component: DashboardComponent},
-{path: 'map', component: MapComponent}];
+{path: 'map', component: DatePickerComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
